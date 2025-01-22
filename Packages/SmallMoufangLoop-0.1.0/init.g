717d37728dd76853c71169354c516aace24e498a
AtleHahn <atle.hahn@gmail.com>
2025-01-22 08:50:04 +0000

    added: package for Moufang loops

---
+++ Packages/SmallMoufangLoop-0.1.0/init.g
@@ -0,0 +1,14 @@
+#############################################################################
+##
+#W    init.g          The SmallMoufangLoop Package           Joao Araújo
+#Y                                                 Mikoláš Janota, Gonçalo Araújo, Choiwah Chow, Atle Hahn
+##
+
+#############################################################################
+##
+#R  Read the declaration files.
+##
+ReadPackage( "SmallMoufangLoop", "lib/SMALLMOUFANGLOOP_utils.gd");
+ReadPackage( "SmallMoufangLoop", "lib/SMALLMOUFANGLOOP_small.gd");
+
+#E  init.g . . . . . . . . . . . . . . . . . . . . . . . . . . . .  ends here
---
+++ Packages/SmallMoufangLoop-0.1.0/lib/SMALLMOUFANGLOOP_small.gd
@@ -0,0 +1,51 @@
+#############################################################################
+##
+#W  small.gd  Small algebra access function for moufangloop
+##  
+#Y  Copyright (C)  2022                            Joao Araújo
+#Y                                                 Mikoláš Janota, Gonçalo Araújo, Choiwah Chow, Atle Hahn
+##  This file is auto-generated
+##
+
+###########################################################################
+##
+##  <#GAPDoc Label="AllSmallMoufangLoop">
+##  <ManSection>
+##  <Func Name="AllSmallMoufangLoop" Arg="m"/>
+##  <Description>
+##  This function returns all the moufangloop of order <M><A>m</A></M> from the library.<P/>
+##
+##  No check is performed to verify that <A>m</A> is a valid argument. <P/>
+##
+##  In <C>AllSmallMoufangLoop</C>, an error will be signaled if the moufangloop
+##  of order <A>m</A> are not available.
+##  <Example><![CDATA[
+##  gap> Length(AllSmallMoufangLoop(2));
+##  1
+##  ]]></Example>
+##  </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+
+DeclareGlobalFunction( "AllSmallMoufangLoop" );
+#############################################################################
+
+###########################################################################
+##
+##  <#GAPDoc Label="IsASmallMoufangLoop">
+##  <ManSection>
+##  <Func Name="IsASmallMoufangLoop" Arg="m"/>
+##  <Description>
+##  This function checks whether <M><A>m</A></M>, which must be a multiplication table, is isomorphic to a MoufangLoop in the library.<P/>
+##
+##  No check is performed to verify that <A>m</A> is a valid argument. <P/>
+##  <Example><![CDATA[
+##  gap> IsASmallMoufangLoop(AllSmallMoufangLoop(2)[1]);
+##  true
+##  ]]></Example>
+##  </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+
+# DeclareGlobalFunction( "IsASmallMoufangLoop" );
+#############################################################################
